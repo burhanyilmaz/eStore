848886962454a25a3b57c8bca1ae5375199a5fda
Burhan YILMAZ <burhan.yilmaaz@gmail.com>
2023-12-20 23:17:32 +0300

    feat: added folder and screens, helpers, and colors files

---
+++ src/screens/ProductDetails/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const ProductDetailsScreen = () => {
+  return (
+    <View>
+      <Text>Product Details Screen</Text>
+    </View>
+  );
+};
+
+export default ProductDetailsScreen;
